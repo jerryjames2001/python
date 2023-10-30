43845fe074d18de32159b854bd43c078b4ca8221
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2023-10-30 15:03:47 +0530

    packages

---
+++ cycle3/32custompackage/graphics/circle.py
@@ -0,0 +1,8 @@
+import math
+pi=math.pi
+def ciarea(r):
+    area=pi*r*r
+    print("Area of circle=",area)
+def ciperimeter(r):
+    perimeter=2*pi*r
+    print("Perimeter of circle=",perimeter)
---
+++ cycle3/32custompackage/graphics/cuboid.py
@@ -0,0 +1,6 @@
+def cuareaa(l,b,h):
+    areaa=l*b*h;
+    print("Area of cuboid=",areaa)
+def cuperimeter(l,b,h):
+    perimeter=4*(l+b+h)
+    print("Perimeter=",perimeter)
