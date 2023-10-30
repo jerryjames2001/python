d2d49bc894bea33b1e08d41106eb11b08a790aea
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2023-10-30 15:00:42 +0530

    Add files via upload

---
+++ cycle3/32custompackage.py
@@ -0,0 +1,15 @@
+from graphics import rectangle,circle,cuboid
+a=int(input("Enter length for rectangle"))
+b=int(input("Enter breadth for rectangle"))
+rectangle.rearea(a,b)
+rectangle.reperimeter(a,b)
+
+i=int(input("Enter length for cuboid"))
+j=int(input("Enter breadth for cuboid"))
+k=int(input("Enter length for cuboid"))
+cuboid.cuareaa(i,j,k)
+cuboid.cuperimeter(i,j,k)
+
+r=int(input("Enter radius for circle"))
+circle.ciarea(r)
+circle.ciperimeter(r)
