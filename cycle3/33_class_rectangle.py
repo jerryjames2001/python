f17e7a6e772f494e3a8434afb7170b5ba8c59139
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2023-11-10 10:27:58 +0530

    33 rectangle class

--- cycle3/33_class_rectangle.py
+++ cycle3/33_class_rectangle.py
@@ -7,8 +7,14 @@ class rectangle:
     def perimeter(self):
         self.p=4 * (self.x + self.y)
     def disp(self):
-        print("Area=",self.a)
-        print("Perimeter=",self.p)
+        print("Area=",self.a," \tPerimeter=",self.p)
+    def comp(self,obj2):
+        if self.a > obj2.a:
+            print("Area of Rectangle 1 is greater than rectangle 2");
+        elif self.a == obj2.a:
+            print("Area of Rectangle 1 is same of rectangle 2");
+        else:
+            print("Area of Rectangle 2 is greater than rectangle 1");
 
 l1=int(input("Enter the length of rectangle 1"))
 b1=int(input("Enter the breadth of rectangle 1"))
@@ -17,10 +23,16 @@ l2=int(input("Enter the length of rectangle 2"))
 b2=int(input("Enter the breadth of rectangle 2"))
 
 obj1=rectangle(l1,b1)
-obj2=rectangle(l2,b2)
+print("Rectangle 1:")
 obj1.area()
 obj1.perimeter()
+obj1.disp()
+
+print("Rectangle 2:")
+obj2=rectangle(l2,b2)
 obj2.area()
 obj2.perimeter()
-obj1.disp()
 obj2.disp()
+
+print("\nComparing area of rectangle \n")
+obj1.comp(obj2);
