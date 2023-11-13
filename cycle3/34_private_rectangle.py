f201da3927c34c6932d800771c19e4895755fd8e
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2023-11-13 15:42:38 +0530

    private class rectangle
    here __ls__ is (less than) meaning used as def

--- cycle3/34_private_rectangle.py
+++ cycle3/34_private_rectangle.py
@@ -2,25 +2,25 @@ class rect:
     def __init__(self,a,b):
         self.l=a
         self.b=b
-        self.area_val=None
     def area(self):
-        self.area=self.l*self.b
-        print("area= ",self.area)
-        return self.area
+        area1=self.l*self.b
+        return area1
     def __lt__(self,obj):
-        if self.area() < obj.area_val():
-            print("Rectancgle 1 less than Rectangle 2")
+        if(self.area() < obj.area()):
+            return "Rectancgle 1 less than Rectangle 2"
         else:
-            print("Rectancgle 2 less than Rectangle 1")
+            return "Rectancgle 2 less than Rectangle 1"
 print("Enter rectangle 1 values")
 x=int(input("Enter length: "))
 y=int(input("Enter breadth: "))
 obj1=rect(x,y)
-area1=obj1.area()
+print("area is:")
+print(obj1.area())
 
 print("Enter rectangle 2 values")
 x=int(input("Enter length: "))
 y=int(input("Enter breadth: "))
 obj2=rect(x,y)
-area2=obj2.area()
-obj1 < obj2
+print("area is:")
+print(obj2.area())
+print(obj1<obj2)
